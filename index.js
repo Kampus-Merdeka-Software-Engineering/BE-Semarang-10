0519e1873ea93d901afa1e9c28cc93be539e417b
Bimamaarschal <bimamaarschal@gmail.com>
2023-09-28 09:52:15 +0700

    Menambahkan page (modul) dokumentasi for DEVELOPER

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
 
 const kursusRoutes = require('./src/routes/kursusRoutes');
 const kontakRoutes = require('./src/routes/kontakRoutes');
@@ -9,11 +16,12 @@ const infoberitaRoutes = require('./src/routes/infoberitaRoutes');
 const datahomeRoutes = require('./src/routes/datahomeRoutes');
 const unggulanRoutes = require('./src/routes/unggulanRoutes');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/dokumentasi', (req, res) => {
+  res.render('index', { title: 'Halaman Dokumentasi XGROW DEVELOPER' });
+});
 
 app.use('/api', kursusRoutes);
 app.use('/api', kontakRoutes);
@@ -21,7 +29,6 @@ app.use('/api', berlanggananRoutes);
 app.use('/api', infoberitaRoutes);
 app.use('/api', datahomeRoutes);
 app.use('/api', unggulanRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
